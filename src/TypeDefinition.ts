2e8ca7944a3d0eb4058c1f700a2046aca473d59b
Beka Westberg <bekawestberg@gmail.com>
2021-09-27 15:23:38 -0400

    Add readonly getters for TypeDefinition properties

--- src/TypeDefinition.ts
+++ src/TypeDefinition.ts
@@ -12,65 +12,84 @@ import {IncompatibleType} from "./exceptions";
 export class TypeDefinition {
   private hierarchy: TypeHierarchy;
   readonly name: string;
-  // TODO: Add readonly accessors.
-  readonly parents: TypeInstantiation[] = [];
-  readonly children: TypeInstantiation[] = [];
-  readonly ancestors: TypeInstantiation[] = [];
-  readonly descendants: TypeInstantiation[] = [];
-  readonly params: ParameterDefinition[] = [];
+  private readonly parents_: TypeInstantiation[] = [];
+  private readonly children_: TypeInstantiation[] = [];
+  private readonly ancestors_: TypeInstantiation[] = [];
+  private readonly descendants_: TypeInstantiation[] = [];
+  private readonly params_: ParameterDefinition[] = [];
 
   constructor(hierarchy: TypeHierarchy, name: string) {
     this.hierarchy = hierarchy;
     this.name = name;
-    this.ancestors.push(this.createInstance());
-    this.descendants.push(this.createInstance());
+    this.ancestors_.push(this.createInstance());
+    this.descendants_.push(this.createInstance());
+  }
+
+  public get parents() {
+    return (this.parents_ as readonly TypeInstantiation[]);
+  }
+
+  public get children() {
+    return (this.children_ as readonly TypeInstantiation[]);
+  }
+
+  public get ancestors() {
+    return (this.ancestors_ as readonly TypeInstantiation[]);
+  }
+
+  public get descendants() {
+    return (this.descendants_ as readonly TypeInstantiation[]);
+  }
+
+  public get params() {
+    return (this.params_ as readonly ParameterDefinition[]);
   }
 
   hasParent(t: TypeInstantiation): boolean {
-    return this.parents.some(p => p.equals(t));
+    return this.parents_.some(p => p.equals(t));
   }
 
   hasChild(t: TypeInstantiation): boolean {
-    return this.children.some(c => c.equals(t));
+    return this.children_.some(c => c.equals(t));
   }
 
   hasAncestor(t: TypeInstantiation): boolean {
-    return this.ancestors.some(a => a.equals(t));
+    return this.ancestors_.some(a => a.equals(t));
   }
 
   hasDescendant(t: TypeInstantiation): boolean {
-    return this.descendants.some(d => d.equals(t));
+    return this.descendants_.some(d => d.equals(t));
   }
 
   addParent(t: TypeInstantiation) {
     if (!this.hierarchy.typeIsCompatible(t)) throw new IncompatibleType(t);
     if (this.hasParent(t)) return;
-    this.parents.push(t);
+    this.parents_.push(t);
     this.addAncestor(t);
     const td = this.hierarchy.getTypeDef(t.name);
-    td.ancestors.forEach(a => this.addAncestor(a));
+    td.ancestors_.forEach(a => this.addAncestor(a));
     td.addChild(this.createInstance());
   }
 
   private addChild(t: TypeInstantiation) {
     if (this.hasChild(t)) return;
-    this.children.push(t);
+    this.children_.push(t);
     this.addDescendant(t);
     const td = this.hierarchy.getTypeDef(t.name);
-    td.descendants.forEach(d => this.addDescendant(d));
+    td.descendants_.forEach(d => this.addDescendant(d));
   }
 
   private addAncestor(t: TypeInstantiation) {
     if (this.hasAncestor(t)) return;
-    this.ancestors.push(t);
-    this.children.forEach(
+    this.ancestors_.push(t);
+    this.children_.forEach(
         c => this.hierarchy.getTypeDef(c.name).addAncestor(t));
   }
 
   private addDescendant(t: TypeInstantiation) {
     if (this.hasDescendant(t)) return;
-    this.descendants.push(t);
-    this.parents.forEach(
+    this.descendants_.push(t);
+    this.parents_.forEach(
         p => this.hierarchy.getTypeDef(p.name).addDescendant(t));
   }
 

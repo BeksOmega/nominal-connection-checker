a0ad9ec7fa345c62f6b14bc02371ccd48fb168ea
Beka Westberg <bekawestberg@gmail.com>
2021-12-09 18:26:11 -0500

    Get simple generic tests working

--- src/connection_typer.ts
+++ src/connection_typer.ts
@@ -4,12 +4,61 @@
  * SPDX-License-Identifier: MIT
  */
 
-import {Connection} from 'blockly';
-import {TypeInstantiation} from './type_instantiation';
+import {Block, Connection} from 'blockly';
+import {TypeHierarchy} from './type_hierarchy';
+import {ExplicitInstantiation, GenericInstantiation, TypeInstantiation} from './type_instantiation';
 import {parseType} from './type_parser';
 
 export class ConnectionTyper {
-  getTypesOfConnection(c: Connection): TypeInstantiation {
+  constructor(
+      readonly hierarchy: TypeHierarchy,
+  ) {}
+
+  getTypesOfConnection(c: Connection): TypeInstantiation[] {
+    const t = this.getCheck(c);
+    // TODO: Handling parameterized yptes.
+    if (t instanceof ExplicitInstantiation) return [t];
+    if (c.isSuperior()) return this.getTypesOfInput(c, t as GenericInstantiation);
+    return this.getTypesOfOutput(c, t as GenericInstantiation);
+  }
+
+  private getTypesOfInput(
+      c: Connection, t: GenericInstantiation
+  ): TypeInstantiation[] {
+    const s = c.getSourceBlock();
+    const pConn = s.outputConnection || s.previousConnection;
+    // TODO: Tests for this.
+    // TODO: tests for when output does not match input type
+    if (!pConn || !pConn.targetConnection) return [new GenericInstantiation('')];
+    const pTypes = this.getTypesOfConnection(pConn.targetConnection);
+    // TODO: How do we handle multiple types?
+    if (pTypes[0] instanceof ExplicitInstantiation) {
+      return [new GenericInstantiation('', [], pTypes)];
+    }
+    return pTypes;
+  }
+
+  private getTypesOfOutput(
+      c: Connection, t: GenericInstantiation
+  ): TypeInstantiation[] {
+    const s = c.getSourceBlock();
+    const matches = this.getInputConnections(s).reduce((acc, c2) => {
+      if (!this.getCheck(c2).equals(t) || !c2.targetConnection) return acc;
+      // TODO: Not sure if we need to do an explicit test like inputs.
+      return [...acc, ...this.getTypesOfConnection(c2.targetConnection)];
+    }, [] as TypeInstantiation[]);
+    if (matches.length) {
+      return this.hierarchy.getNearestCommonAncestors(...matches);
+    }
+    return [new GenericInstantiation(
+        '', t.unfilteredLowerBounds, t.unfilteredUpperBounds)];
+  }
+
+  private getInputConnections(b: Block): Connection[] {
+    return b.inputList.map(i => i.connection);
+  }
+
+  private getCheck(c: Connection) {
     return parseType(c.getCheck()[0]);
   }
 }
